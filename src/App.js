3e889b779e9db14c52d4aa2a91fdda3decbcc982
Manfrinmm <matheus_mm123@hotmail.com>
2021-04-14 19:25:57 -0300

    add google analytics

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import React from "react";
-
-import GlobalStyle from "./styles/global";
+import React, { useEffect } from "react";
+import ReactGA from "react-ga";
 
 import Home from "./pages/Home";
+import GlobalStyle from "./styles/global";
 
 function App() {
+  useEffect(() => {
+    if (process.env.NODE_ENV !== "development") {
+      ReactGA.initialize("G-DZEBZT5ZN5");
+      ReactGA.pageview(window.location.pathname + window.location.search);
+
+      console.log("Analytics is enabled");
+    }
+  }, []);
   return (
     <>
       <GlobalStyle />
